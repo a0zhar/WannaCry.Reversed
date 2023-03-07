aadcdf86c2469b4c2fee7843730d8ac898028995
̸̨̡͉̯̟͇̗͈͙͙̝͍̽̾̒Ă̶̺̝̞̊́́̏̈͌̾̍̀̈́̂̋̔̇͐̔̀͘̚͜ ̴̢͈̜̦͔̦̦̖̗͚̦̈́ͅZ̶̫̥̭͎̝̻̮̘̯̳̿ͅ ̴͈̅͊͒͂͌̆͂͠͠H̴̨̛̺͕̳̱͙̩̻̩̖̲͙̘̓̾̌ͅͅ ̷͎̥̖̬̼͎̹̼̭̺̪̯̱̥̖̄̊̈́̀̃̀̂̐͊̐̄̄́̅͂̈́̋̚͠͝À̸̢̝̖͇̤̹̥͙͎̳̼̬̲̰̼̩͛̀̒̋͛̈́̊ ̴̣̹̩̥̺̙̪͎͕̭̟̲͇̘̺̈́̎̿̇́͝R̷̢̢̧̡̢̲̺̳̖̹͈̙̰̙̥̼̗͍̒̌ͅ <96749372+a0zhar@users.noreply.github.com>
2023-03-07 13:49:09 +0100

    formatted + grammar fix

--- encryptorDLL/dllmain.cpp
+++ encryptorDLL/dllmain.cpp
@@ -1,30 +1,18 @@
-#include <Windows.h>
+#include <windows.h>
 
-//help from:
-//https://www.youtube.com/watch?v=ru5VzUigKqw
+/* help from: https://www.youtube.com/watch?v=ru5VzUigKqw
+compile and embed in the main project. This file is loaded 
+by Wannacry for the encryption portion. */
 
-//compile and embed in main project
-//this file is loaded by Wannacry for the encryption portion
-
-BOOL WINAPI DllMain(HINSTANCE hModule, DWORD dwReason, LPVOID)
-{
-
-    switch (dwReason)
-    {
-    case DLL_PROCESS_ATTACH:
-    {
-        MessageBoxA(NULL, "DLL_PROCESS_ATTACH", "DLL_PROCESS_ATTACH", MB_OK);
-    }
-    break;
-    case DLL_PROCESS_DETACH:
-    {
-        //detach
-    }
-    break;
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
+BOOL WINAPI DllMain(HINSTANCE hModule, DWORD dwReason, LPVOID) {
+    switch (dwReason) {
+      case DLL_PROCESS_ATTACH: MessageBoxA(NULL, "DLL_PROCESS_ATTACH", "DLL_PROCESS_ATTACH", MB_OK); break;
+      case DLL_PROCESS_DETACH:
+      //detach here
+      break;
+      case DLL_THREAD_ATTACH:
+      case DLL_THREAD_DETACH:
         break;
     }
-
     return TRUE;
 }
